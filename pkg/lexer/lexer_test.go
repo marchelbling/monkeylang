f728a885df39e0b9b1ebaa4ed3ade65a553e6e9e
Marc Helbling <marc.d.helbling@gmail.com>
2025-12-05 20:52:01 +0100

    feat: implement basic lexer

---
+++ pkg/lexer/lexer_test.go
@@ -0,0 +1,68 @@
+package lexer
+
+import (
+	"testing"
+
+	"github.com/marchelbling/monkeylang/pkg/token"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `let five = 5;
+let ten = 10;
+
+let add = fn(x, y) {
+	x + y;
+};
+
+let result = add(five, ten);
+`
+	l := New(input)
+	for _, tc := range []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.LET, "let"},
+		{token.IDENT, "five"},
+		{token.ASSIGN, "="},
+		{token.INT, "5"},
+		{token.SEMILCOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMILCOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
+		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMILCOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMILCOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "five"},
+		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
+		{token.SEMILCOLON, ";"},
+		{token.EOF, ""},
+	} {
+		tok := l.NextToken()
+
+		require.Equal(t, tok.Type, tc.expectedType)
+		require.Equal(t, tok.Literal, tc.expectedLiteral)
+	}
+}
